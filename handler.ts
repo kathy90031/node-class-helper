760ab3b874c0b9f39d2d37b119ae2fb510efeb95
DESKTOP-A6MTM32\young <youngblood.katherine@gmail.com>
2018-05-15 11:49:20 -0400

    added aws-lambda types

--- handler.ts
+++ handler.ts
@@ -1,6 +1,7 @@
 import { APIGatewayEvent, Callback, Context, Handler } from 'aws-lambda';
 
 export const hello: Handler = (event: APIGatewayEvent, context: Context, cb: Callback) => {
+  
   const response = {
     statusCode: 200,
     body: JSON.stringify({
